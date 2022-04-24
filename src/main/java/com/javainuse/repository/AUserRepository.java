5e9f03fdc7ec8f4f59a94a790db1a5f162ebfe87
marcelthijssen <info@studiothijssen.nl>
2022-04-24 16:02:13 +0200

    jwt+jpa

--- src/main/java/com/javainuse/repository/AUserRepository.java
+++ src/main/java/com/javainuse/repository/AUserRepository.java
@@ -1,11 +1,12 @@
 package com.javainuse.repository;
 
 import com.javainuse.model.AUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AUserRepository extends CrudRepository<AUser, Integer> {
+public interface AUserRepository extends JpaRepository<AUser, Integer> {
 	
 	AUser findByUsername( String username);
 	
